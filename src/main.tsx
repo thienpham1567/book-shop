a16874b76bf976db27378d746d57fba218f86c5a
thienpham1567 <thienphamuser@gmail.com>
2022-08-17 12:12:31 +0700

    initial home page

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { store } from './app/store'
 import { Provider } from 'react-redux';
 import App from './App'
-import Hero from './components/Hero/Hero'
 import Fiction from './pages/Categories/Fiction/Fiction';
-
+import Home from './pages/Home/Home';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
@@ -15,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
       <Provider store={store}>
         <Routes>
           <Route path='/' element={<App />}>
-            <Route path='/' element={<Hero />} />
+            <Route index path='/' element={<Home />} />
             <Route path='/categories/fiction' element={<Fiction />} />
           </Route>
         </Routes>
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,27 @@
 import React, { useState, useEffect } from 'react'
 import './Home.scss'
+import Carousel from 'react-bootstrap/Carousel';
+import Banner1 from '../../assets/image/banner1.jpg';
+import Banner2 from '../../assets/image/banner2.jpg';
+import Banner3 from '../../assets/image/banner3.jpg';
 
+const banners = [Banner1, Banner2, Banner3];
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <Carousel>
+        {
+          banners.map((banner) => (<Carousel.Item interval={3000}>
+            <img
+              className="d-block w-100"
+              src={banner}
+              alt="First slide"
+            />
+          </Carousel.Item>))
+        }
+      </Carousel>
+
+    </>
   )
 }
 
